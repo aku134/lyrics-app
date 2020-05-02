073b77d05da2374786f81d5edab1b92f97d9b50c
alias-rahil <59060219+alias-rahil@users.noreply.github.com>
2020-05-02 21:20:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.all('*', async (req, res) => {
     if (req.body.artist && req.body.song) {
     let artist = req.body.artist
     let song = req.body.song
-    url = `https://orion.apiseeds.com/api/music/lyric/${artist}/${song}/?apikey=5PTn1ArO77QZVfLHoIjPNEg4RBX0WrmDTcjvYByKNcu9l5AcDYsBL2aLJBPldKqJ`
+    let url = `https://orion.apiseeds.com/api/music/lyric/${artist}/${song}/?apikey=5PTn1ArO77QZVfLHoIjPNEg4RBX0WrmDTcjvYByKNcu9l5AcDYsBL2aLJBPldKqJ`
     url = await tiny.get({ url })
     url = url.body.result.track.text
     if (url) { res.render("lyric", {url: url}) } else { res.render("not found", {url: "not found"}) }} else {res.render("search", {url: false})}
