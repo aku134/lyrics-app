1c65812a83983e794ad9d7a1ce8f2a21b4323574
aku134 <60403682+aku134@users.noreply.github.com>
2020-05-02 21:43:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.all('*', async (req, res) => {
     let url = `https://orion.apiseeds.com/api/music/lyric/${artist}/${song}/?apikey=5PTn1ArO77QZVfLHoIjPNEg4RBX0WrmDTcjvYByKNcu9l5AcDYsBL2aLJBPldKqJ`
     url = await tiny.get({ url })
     url = url.body.result.track.text
-    if (url) { res.render("lyric", {url: url}) } else { res.render("not found",  {url: false})}
+    if (url) { res.render("lyric", {url: url}) } else { res.render("not found",  {url: "not found"})}else {res.render("search",{url:"false"})}
     
   
 
