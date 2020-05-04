70584a5dc5cb9434a6f0a12779e5b52b619f92c6
alias-rahil <59060219+alias-rahil@users.noreply.github.com>
2020-05-04 11:50:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-const express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    lyrics = require("simple-get-lyrics");
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const lyrics = require("simple-get-lyrics")
 app.use(bodyParser.urlencoded({
-    extended: !0
-})), app.set("view engine", "ejs"), app.use(express.static("./public")), app.all("*", async (e, r) => {
-    if (e.body.artist && e.body.song) {
-        let p = e.body.artist,
-            a = e.body.song;
+    extended: true
+}))
+app.set('view engine', 'ejs')
+app.use(express.static('./public'));
+
+
+
+
+app.all('*', async (req, res) => {
+
+    if (req.body.artist && req.body.song) {
+        let artist = req.body.artist
+        let song = req.body.song
         try {
-            let s = (s = await lyrics.search(`${p}`, `${a}`)).lyrics
-            r.render("lyric", {
-            url: s
-        })
+            let url = url = await lyrics.search(`${artist}`, `${song}`)
+            url = url.lyrics
+            res.render("lyric", {
+                url: url
+            })
         } catch (e) {
-         r.render("not found", {
-            url: "not found"
-        })}
-    } else r.render("search", {
-        url: !1
-    })
-}), app.listen(process.env.PORT || "2000");
+            res.render("not found", {
+                url: "not found"
+            })
+        }
+    } else {
+        res.render("search", {
+            url: false
+        })
+    }
+
+
+
+
+});
+app.listen(process.env.PORT || "2000")
